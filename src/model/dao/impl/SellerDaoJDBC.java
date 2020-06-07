6d90157fe4d6574f307d8e105801c681563e73ec
gabs-dev <gabrielfelipe1458@gmail.com>
2020-06-07 17:01:32 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller s) {
+
+    }
+
+    @Override
+    public void update(Seller s) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+
+}
